d2f7116cdd5f95092d5097599e469976f45c3ce8
solangehelenam3-hub <solangehelenam3@gmail.com>
2026-04-16 20:37:49 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <header>
-      <h1>Alunísio Silva</h1>
-      <p>Especialista em Empréstimos</p>
+      <h1>Solange Maria de Moura da Silva</h1>
+      <p>Estudante do Curso de Sistemas para Internet</p>
     </header>
   );
 }
